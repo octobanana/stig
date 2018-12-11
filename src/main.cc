961bff50c5628d8d899fec004c2ddfaa342cf90b
Brett Robinson <octo.banana93@gmail.com>
2018-12-10 17:30:16 -0700

    update version

--- src/main.cc
+++ src/main.cc
@@ -14,7 +14,7 @@ int program_options(Parg& pg);
 
 int program_options(Parg& pg)
 {
-  pg.name("stig").version("0.0.0 (10.12.2018)");
+  pg.name("stig").version("0.1.0 (10.12.2018)");
   pg.description("A CLI tool for searching Git repositories on GitHub.");
   pg.usage("[flags] [options] [--] [arguments]");
   pg.usage("[-q|--query str] [-p|--page int] [-n|--number int] [-s|--sort stars|forks|updated|best] [-o|--order asc|desc] [-f|--filter key:value[ key:value]...] [--token str] [-c|--color on|off|auto]");
